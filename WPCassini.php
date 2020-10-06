930aee0a56f74aa3b0459b8e47276ffc6e4f6c52
Thomas Vias <t.vias@pixelscodex.com>
2020-10-06 11:05:20 +0200

    First commit

---
+++ WPCassini.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: WPCassini
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A plugin to display maps
+Version: 1.0
+Author: Thomas Vias
+Author URI: https://pixelscodex.com
+License: A "Slug" license name e.g. GPL2
+*/
